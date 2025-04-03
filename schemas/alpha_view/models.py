6d923695917ab54700c2f7f9bfe2f575cc58b90e
yonatanblum <yonatanblum1@gmail.com>
2025-04-03 11:35:07 +0300

    change the format and model schema of alpha view

--- schemas/alpha_view/models.py
+++ schemas/alpha_view/models.py
@@ -1,10 +1,10 @@
 from pydantic import BaseModel, Field
-from typing import List, Dict
+from typing import List, Dict, Union
 
 
 class TokenRequest(BaseModel):
-    chain: int
-    amount: int
+    chain: Union[int, str] = Field(..., description="Chain ID or chain name")
+    amount: int = Field(..., description="Number of smart wallets")
     tokenName: str
     tokenAddress: str
     tokenSymbol: str
